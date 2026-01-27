c1ec2ed3cdc68c8259b2949255d748f0f5f68bdb
Reza Fahlevi <60283386+RezaFahlevi28@users.noreply.github.com>
2026-01-27 20:45:50 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#E54ED0",
+        title: "#0B0A2F",
+        bodyText: "#718096",
+        bodyDark: "#F9FAFB",
+      },
+    },
+  },
+  darkMode: "class",
+  mode: "jit",
+  plugins: [],
+};
